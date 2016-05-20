89ba837fad212274000248282154a1ee26bb1ad5
Jeremiah Wala <jwala@broadinstitute.org>
2016-05-20 18:20:17 -0400

    testing travis build with no compiled libraries

--- .travis.scripts/travis-install.sh
+++ .travis.scripts/travis-install.sh
@@ -5,7 +5,7 @@ set -o pipefail
 
 if [ -d "${BOOST_ROOT}" ]; then
   (cd "${BOOST_ROOT}"
-    ./bootstrap.sh ###--with-libraries="${BOOST_LIBS}"
+    ./bootstrap.sh --with-libraries="${BOOST_LIBS}"
     ./b2 threading=multi --prefix="${BOOST_ROOT}" -d0 install
   )
 fi
