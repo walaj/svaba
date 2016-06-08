3da226f4d56526f2394e6e5e3f5c4a0e1f7d9385
Jeremiah Wala <jwala@broadinstitute.org>
2016-06-08 00:53:34 -0400

    minor bug fixes to v93

--- src/Snowman/SnowmanAssemblerEngine.cpp
+++ src/Snowman/SnowmanAssemblerEngine.cpp
@@ -151,32 +151,13 @@ void SnowmanAssemblerEngine::doAssembly(ReadTable *pRT, ContigVector &contigs, i
   int min_overlap = m_min_overlap;
   int cutoff = 0;
 
-  if (pass > 0) {
-    //min_overlap = m_min_overlap * 1.5;
-    errorRate = 0.03;
-  } 
-  if (pass == 2) 
-    min_overlap = 20;
+  if (pass > 0) 
+    errorRate = 0;
       
-
-  /*
-  if (pass == 0) // || pass == 1)
-    cutoff = 50; //m_readlen;// + 10;
-  //cutoff = 0; // debug
-  else {
-    min_overlap = 35;
-    errorRate = 0.05;
-    cutoff = m_readlen * 1.10;
-  }
-  */
   bool exact = errorRate < 0.001f;
 
   // remove duplicates if running in exact mode
-  ReadTable * pRT_nd;
-  if (exact)
-    pRT_nd = removeDuplicates(pRT);    
-  else 
-    pRT_nd = pRT;
+  ReadTable * pRT_nd = exact ? removeDuplicates(pRT) : pRT;    
 
   // forward
   SuffixArray* pSAf_nd = new SuffixArray(pRT_nd, 1, false); //1 is num threads. false is silent/no
@@ -201,7 +182,7 @@ void SnowmanAssemblerEngine::doAssembly(ReadTable *pRT, ContigVector &contigs, i
   int seedStride = 0;
   if (!exact)
     calculateSeedParameters(m_readlen, min_overlap, seedLength, seedStride);
-
+  
   SnowmanOverlapAlgorithm* pOverlapper = new SnowmanOverlapAlgorithm(pBWT_nd, pRBWT_nd, 
 								     errorRate, seedLength,
 								     seedStride, bIrreducibleOnly);
@@ -385,9 +366,9 @@ void SnowmanAssemblerEngine::remove_exact_dups(ContigVector& cc) const {
 void SnowmanAssemblerEngine::print_results(const ContigVector& cc) const {
 
   if (cc.size() >= 1) {
-    std::cerr << "Contig Count: " << cc.size() << " at " << m_id << std::endl;
+    std::cout << "Contig Count: " << cc.size() << " at " << m_id << std::endl;
     for (auto& i : cc) 
-    	std::cerr << "   " << i.getID() << " " << i.getSeq().length() << " " << i.getSeq() << std::endl;
+    	std::cout << "   " << i.getID() << " " << i.getSeq().length() << " " << i.getSeq() << std::endl;
   }
 
 }
